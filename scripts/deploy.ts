37c2bc52790a1637e99f9ef621a04bfa86591794
Evans007 <ebounce500@gmail.com>
2023-02-17 19:25:10 +0100

    initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  
+  const [owner] = await ethers.getSigners();
+  const nft = await ethers.getContractFactory("NFT");
+  const Nft = await nft.deploy();
+  await Nft.deployed();
+
+
+  console.log(`Deployed to: ${Nft.address}`);
+
+  ////////////// SAFEMINT  ////////////////////
+  const deployer = "0x58DcC051286728071423b907cF5eE1b8A40AC839";
+  const token_id = 10010;
+  const token_uri ="QmWtuqk8fzNh6Lfk9ezWkS9jdQyy6iBQNrev8FfDphLdYG";
+
+  const mint = await Nft.safeMint(deployer,token_id,token_uri);
+  console.log("NFT minted successfully");
+  
+  
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
